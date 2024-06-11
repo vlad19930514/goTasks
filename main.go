fc1e600a22d59e620f40ad987ceb2a117a030638
Vladislav Moskalenko <62288939+vlad19930514@users.noreply.github.com>
2024-06-11 10:17:32 +0300

    first task

--- main.go
+++ main.go
@@ -2,52 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	url := "http://localhost123.com "
-
-	replaceUtl := replaceUtlT{
-		replaceValue: "*",
-		startsWith:   "http://",
+	//go run main.go "Hello, its my page: http://localhost123.com See you"
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a string argument.")
+		return
 	}
-
-	fmt.Println(replaceUtl.Replace(url))
-
+	fmt.Println(CheckLinks(os.Args[1]))
 }
 
-type replaceUtlT struct {
-	replaceValue string
-	startsWith   string
-}
-
-func (data replaceUtlT) compareBites(first []byte, second []byte) bool {
-	for i := range second {
-		if first[i] != second[i] {
-			return false
+func CheckLinks(str string) string {
+	url := []byte(str)
+	values := []byte("http://")
+	asterisk := []byte("*")[0]
+	space := []byte(" ")[0]
+	pointer := 0
+	for i := range url {
+		if pointer == len(values)-1 {
+			if url[i] == space {
+				pointer = 0
+				continue
+			}
+			url[i] = asterisk
+			continue
+		}
+		if url[i] == values[pointer] {
+			pointer++
+		} else {
+			pointer = 0
 		}
 	}
-	return true
-}
 
-func (data replaceUtlT) Replace(urlValue string) string {
-
-	replaceValue := []byte(data.replaceValue)[0]
-	url := []byte(urlValue)
-	start := []byte(data.startsWith)
-
-	shouldBeReplaced := data.compareBites(url[:len(start)], start)
-
-	if shouldBeReplaced {
-		result := make([]byte, 0, len(url))
-		result = append(result, start...)
-		replaceFrom := len(start) - 1
-
-		for i := replaceFrom; i < len(url); i++ {
-			result = append(result, replaceValue)
-		}
-		return string(result)
-	}
-
-	return urlValue
+	return string(url)
 }
